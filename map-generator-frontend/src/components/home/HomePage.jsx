9a786e120e3e96f0ca8b8dd3db857bcfa815b1ae
Viki <viktoriadanevas2003s@gmail.com>
2025-06-16 16:58:03 +0300

    added fading animation added to the home page

--- map-generator-frontend/src/components/home/HomePage.jsx
+++ map-generator-frontend/src/components/home/HomePage.jsx
@@ -1,22 +1,38 @@
-import { Link } from "react-router";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"; 
 import "./HomePage.css";
 
 export default function Home() {
-    return(
-         <>
-      <div className="background-home">
-        <div className="home-overlay">
-          <h1>
-            Welcome to DnD map generator.
-            <br />Perfect for Game Masters.
-          </h1>
-          <p>You can start your map by clicking the button below!</p>
-          <Link to="/biomes">
-            <button className="home-cta-button">Start</button>
-          </Link>
-        </div>
+  const [hoveredSubmit, setHoveredSubmit] = useState(false);
+  const [isFading, setIsFading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    setIsFading(true); 
+    setTimeout(() => {
+      navigate("/biomes");
+    }, 700); 
+  };
+
+  return (
+    <div className={`background-home ${hoveredSubmit ? "bg-tint" : ""}`}>
+      <div className="home-overlay">
+        <h1>
+          Welcome to DnD map generator.
+          <br />Perfect for Game Masters.
+        </h1>
+        <p>You can start your map by clicking the button below!</p>
+        <button
+          className="home-cta-button"
+          onMouseEnter={() => setHoveredSubmit(true)}
+          onMouseLeave={() => setHoveredSubmit(false)}
+          onClick={handleStart}
+        >
+          Start
+        </button>
       </div>
-    </>
-    )
-   
+
+      <div className={`fade-overlay ${isFading ? "active" : ""}`}></div>
+    </div>
+  );
 }
