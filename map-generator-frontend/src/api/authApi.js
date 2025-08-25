d064705a6406bf56b841fc0bcb527f8fce7580c6
Viki <viktoriadanevas2003s@gmail.com>
2025-08-25 19:01:54 +0300

    added auth api

---
+++ map-generator-frontend/src/api/authApi.js
@@ -0,0 +1,120 @@
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import request from "../utils/request";
+import { UserContext, useUserContext } from "../context/UserContext";
+
+const baseUrl = `http://localhost:8080/api`;
+
+export const editProfile = async (userId, updatedData, accessToken) => {
+  try {
+    const options = {
+      headers: {
+        "X-Authorization": accessToken,
+      },
+    };
+
+    return await request.put(
+      `${baseUrl}/users/profile/edit/${userId}`,
+      updatedData,
+      options
+    );
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    throw error;
+  }
+};
+
+export const useProfile = () => {
+  const { userId, accessToken, userLoginHandler } = useUserContext();
+
+  const fetchProfile = async () => {
+    if (!userId || !accessToken) {
+      console.warn("Skipping fetchProfile(): Missing userId or accessToken", {
+        userId,
+        accessToken,
+      });
+      return null;
+    }
+
+    try {
+      const options = {
+        headers: {
+          "X-Authorization": accessToken,
+        },
+      };
+
+      const profileData = await request.get(
+        `${baseUrl}/users/profile/${userId}`,
+        options
+      );
+
+      if (!profileData || !profileData._id) {
+        console.warn("Skipping userLoginHandler(): Missing _id", profileData);
+        return;
+      }
+
+      userLoginHandler({ ...profileData, accessToken });
+
+      return profileData;
+    } catch (error) {
+      console.error("Error fetching profile data:", error);
+      return null;
+    }
+  };
+
+  return { fetchProfile };
+};
+
+export const useLogin = () => {
+  const login = async (email, password) =>
+    request.post(`${baseUrl}/login`, { email, password });
+
+  return {
+    login,
+  };
+};
+
+export const useRegister = () => {
+  const register = (
+    username,
+    email,
+    phoneNumber,
+    imageUrl,
+    address,
+    password
+  ) =>
+    request.post(`${baseUrl}/register`, {
+      username,
+      email,
+      phoneNumber,
+      imageUrl,
+      address,
+      password,
+    });
+
+  return {
+    register,
+  };
+};
+
+export const useLogout = () => {
+  const { accessToken, userLogoutHandler } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+
+    const options = {
+      headers: {
+        "X-Authorization": accessToken,
+      },
+    };
+
+    request.post(`${baseUrl}/logout`, null, options).then(userLogoutHandler);
+  }, [accessToken, userLogoutHandler]);
+
+  return {
+    isLoggedOut: !!accessToken,
+  };
+};
