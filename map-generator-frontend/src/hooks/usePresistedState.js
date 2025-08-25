9689b09e97e3a379bf97755bbbabb9832b175c98
Viki <viktoriadanevas2003s@gmail.com>
2025-08-25 18:58:03 +0300

    added functionality for authentication

---
+++ map-generator-frontend/src/hooks/usePresistedState.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function usePersistedState(stateKey, initialState) {
+  const [state, setState] = useState(() => {
+    const persistedState = localStorage.getItem(stateKey);
+    if (!persistedState) {
+      console.log(
+        `No persisted state found for [${stateKey}]. Using initial state.`
+      );
+      return typeof initialState === "function" ? initialState() : initialState;
+    }
+
+    const persistedStateData = JSON.parse(persistedState);
+    console.log(
+      `Loaded state from localStorage [${stateKey}]:`,
+      persistedStateData
+    );
+
+    return persistedStateData;
+  });
+
+  const setPersistedState = (input) => {
+    const data = typeof input === "function" ? input(state) : input;
+
+    console.log(`Saving state to localStorage [${stateKey}]:`, data);
+
+    localStorage.setItem(stateKey, JSON.stringify(data));
+    setState(data);
+  };
+
+  return [state, setPersistedState];
+}
---
+++ map-generator-frontend/src/providers/UserProvider.jsx
@@ -0,0 +1,41 @@
+import { UserContext } from "../context/UserContext";
+import usePersistedState from "../hooks/usePresistedState";
+
+export default function UserProvider({ children }) {
+  const [authData, setAuthData] = usePersistedState("auth", {});
+
+  const userLoginHandler = (resultData) => {
+    if (!resultData || typeof resultData !== "object") {
+      console.error("Error: Invalid resultData format!", resultData);
+      return;
+    }
+
+    if (!("_id" in resultData)) {
+      console.error(
+        "Error: User ID (_id) is missing in login data!",
+        resultData
+      );
+      return;
+    }
+
+    const formattedData = {
+      ...resultData,
+      userId: resultData._id.toString(),
+    };
+
+    setAuthData(formattedData);
+  };
+
+  const userLogoutHandler = () => {
+    console.log("Logging out...");
+    setAuthData({});
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ ...authData, userLoginHandler, userLogoutHandler }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
