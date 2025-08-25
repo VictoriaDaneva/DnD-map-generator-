299e95fcb2fb990a23dcbaecf930c3fbce70f2c7
Viki <viktoriadanevas2003s@gmail.com>
2025-08-25 19:10:17 +0300

    added guards in the routes

--- map-generator-frontend/src/App.jsx
+++ map-generator-frontend/src/App.jsx
@@ -11,24 +11,33 @@ import Register from "./components/user/register/register";
 import Profile from "./components/user/profile/Profile";
 import PostsPage from "./components/posts/PostsPage";
 import PostPage from "./components/postPage/PostPage";
+import UserProvider from "./providers/UserProvider";
+import GuestGuard from "./guards/GuestRouter";
+import AuthGuard from "./guards/AuthGuard";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/biomes" element={<Biomes />} />
         <Route path="/posts" element={<PostsPage />} />
         <Route path="/post/nesh" element={<PostPage />} />
-        <Route path="/biomes/grassland" element={<Grassland />} />
-        <Route path="/biomes/desert" element={<Desert />} />
-        <Route path="/biomes/tundra" element={<Tundra />} />
+
+        <Route element={<AuthGuard />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/biomes/grassland" element={<Grassland />} />
+          <Route path="/biomes/desert" element={<Desert />} />
+          <Route path="/biomes/tundra" element={<Tundra />} />
+        </Route>
+
+        <Route element={<GuestGuard />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
-    </>
+    </UserProvider>
   );
 }
 
