41357381a97349a601266efdae2ef7900fc41050
Viki <viktoriadanevas2003s@gmail.com>
2025-08-25 18:55:32 +0300

    added request to the server

---
+++ map-generator-frontend/src/utils/request.js
@@ -0,0 +1,37 @@
+const request = async (method, url, data, options = {}) => {
+  options.method = method;
+
+  if (!options.headers) {
+    options.headers = {};
+  }
+
+  if (data && method !== "GET" && method !== "DELETE") {
+    options = {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+      body: JSON.stringify(data),
+    };
+  }
+
+  console.log(`🔍 ${method} Request to ${url}`, options);
+
+  const response = await fetch(url, options);
+  const responseContentType = response.headers.get("Content-Type");
+
+  if (!responseContentType) {
+    return;
+  }
+
+  const result = await response.json();
+  return result;
+};
+
+export default {
+  get: (url, options = {}) => request("GET", url, null, options),
+  post: request.bind(null, "POST"),
+  put: request.bind(null, "PUT"),
+  delete: (url, options = {}) => request("DELETE", url, null, options),
+};
